37547b40a81728b8c24b7c53788b567001b288d5
nicostap <nicoalexandrekurniawan@gmail.com>
2025-07-09 20:29:32 +0700

    fix

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='torch_pruning_ot',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where='.'),
+    package_dir={'': '.'},
     install_requires=[
         'torch',
         'numpy',
         'scikit-learn',
         'POT'
     ],
-    python_requires='>=3.6',
 )
