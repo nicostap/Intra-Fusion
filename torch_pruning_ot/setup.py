73700164144e14294a88733eff1ce659cb610665
nicostap <nicoalexandrekurniawan@gmail.com>
2025-07-09 20:08:03 +0700

    make pip installable

---
+++ torch_pruning_ot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='torch_pruning_ot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'torch', 'numpy', 'scikit-learn', 'POT'
+    ],
+)
